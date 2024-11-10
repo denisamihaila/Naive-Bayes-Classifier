979fef8470abdc0b7867b47d24f7901a5645e438
bgd <trifan.bogdan09@yahoo.com>
2024-11-10 19:39:33 +0200

    Adaugate mai multe articole Acuratetea in acest commit este 65.51%

--- main2.py
+++ main2.py
@@ -1,82 +1,96 @@
 import json
-from collections import Counter
 
-# functia read_data() citeste articolele din json-ul articole.json
-# si face un dictionar de forma sport_dict[nume_sport] = [ cuvinte ]
-# unde nume_sport este numele fiecarui sport care se poate regasi in
-# json-ul citit (in cazul acesta 'football', 'basketball', 'handball',
-# 'tennis' si [ cuvinte ] este lista tuturor cuvintelor din toate
-# articolele corespunzatoare unui sport (cuvintele nu sunt unice - cred)
+delimiters = [",", ";", " ", "'", "’", "-", "."]
+probability_of_a_sport = 0
+sports = []
+
+# Functia citeste datele din articole.json
+# Prelucreaza datele si la final returneaza un tuplu
+# organized_words_by_sport este un dictionar de forma organized_words_by_sport[nume_sport] = [cuvintele din sportul respectiv]
+# all_words este o lista cu TOATE cuvintele din toate articolele
 def read_data():
     fin = open('articole.json', 'r', encoding='utf-8')
     data = json.load(fin)
     fin.close()
 
-    sport_dict = dict()
-    delimiters = [",", ";", " ", "'", "’", "-", "."]
-    for sport in data:
-        all_words = []
-        for articol in data[sport]:
-            text = articol["text"]
+    global sports
+    sports = [sport for sport in data]
+
+    organized_words_by_sport = dict()
+    all_words = set()
+
+    for sport in sports:
+        organized_words_by_sport[sport] = list()
+        for field in data[sport]:
+            text = field["text"]
             for delimiter in delimiters:
-                text = " ".join(text.split(delimiter))
-            words = [word.lower() for word in text.split() if len(word) > 2]
-            all_words.extend(words)
+                text = text.replace(delimiter, " ")
+            text = text.split()
 
-        sport_dict[sport] = all_words
+            for word in text:
+                organized_words_by_sport[sport].append(word.lower())
+                all_words.add(word.lower())
 
-    return sport_dict
+    global probability_of_a_sport
+    probability_of_a_sport = 1 / len(sports)
 
-# functia primeste dictionarul cu cuvinte
-# si calculeaza probabilitatea fiecare cuvant
-# sa apara intr-o categorie de sport
-def calculate_probabilities_word(sport_dict):
-    word_prob = dict()
-    for sport in sport_dict: # football, handball, tennis, basketball
-        word_prob[sport] = dict()
-        total_words = len(sport_dict[sport])
-        used_words = set()
+    return (organized_words_by_sport, all_words)
 
-        for word in sport_dict[sport]:
-            if word not in used_words:
-                appearances = sport_dict[sport].count(word)
-                word_prob[sport][word] = appearances / total_words
-                used_words.add(word)
-    return word_prob
+# Functia asta imi intoarce o structura de date
+# din care pot lua probabilitatea unui cuvant
+# in cadrul unui sport in O(1)
+# exemplu: probability_of_word["Manchester"]["football"] = 0.013
+def probabilities_of_words(tpl):
+    organized_words_by_sport = tpl[0]
+    all_words = tpl[1]
 
-def classify_text(text, probs):
-    delimiters = [",", ";", " ", "'", "’", "-", "."]
+    probability_of_word = { word: dict() for word in all_words }
+
+    for word in all_words:
+        for sport in organized_words_by_sport:
+            # Numar de cate ori apare un cuvant din TOATA lista de cuvinte disponibile
+            # in cadrul fiecarui sport (tennis, football, handball, basketball)
+            word_frequency = organized_words_by_sport[sport].count(word)
+
+            # Probabilitatea unui cuvant este data de (de_cate_ori_a_aparut_acel_cuvant + 1) / (toate cuvintele din acel sport)
+            # + 1 ca sa nu avem probabilitate 0
+            probability_of_word[word][sport] = (word_frequency + 1) / (len(organized_words_by_sport[sport]))
+
+    return probability_of_word
+
+def classify_prompt(prompt, probability_of):
+    global sports, probability_of_a_sport
+
     for delimiter in delimiters:
-        text = text.replace(delimiter, " ")
-    words = [word.lower() for word in text.split() if len(word) > 2]
-
-    # P(category)
-    p_category = 1 / len(probs)
+        prompt = prompt.replace(delimiter, " ")
+    words = [word.lower() for word in prompt.split()]
 
     result = (0, "")
 
-    for category in probs:
-
-        # Compute P(word)
-        P_word = 0.0001
-        for word in words:
-            P_word += words.count(word) / len(words)
-
-        # Compute P(word | category)
-        P_word_given_category = 0.0001
-        for word in words:
-            if word in probs[category]:
-                P_word_given_category += probs[category][word]
-
-        # Compute P(category | word)
-        p_category_given_word = P_word_given_category * p_category / P_word
-
-        if p_category_given_word >= result[0]:
-            result = (p_category_given_word, category)
+    for word in words:
+        for sport in sports:
+            if word in probability_of:
+                probability = probability_of_a_sport * probability_of[word][sport]
+                if probability > result[0]:
+                    result = (probability, sport)
 
     return result
 
-data = read_data()
-probs = calculate_probabilities_word(data)
-res = classify_text("I've won this tennis match using my FIFA Manchester City team", probs)
-print(f"Textul apartine categoriei {res[1]} avand probabilitatea de {res[0] * 100:.2f}%.")
+def calculate_accuracy():
+    tpl = read_data()
+    organized_words_by_sport = tpl[0]
+
+    probability_of = probabilities_of_words(tpl)
+
+    count = 0
+    for sport in organized_words_by_sport:
+        count += len(organized_words_by_sport[sport])
+
+    correct = 0
+    for sport in organized_words_by_sport:
+        for text in organized_words_by_sport[sport]:
+            correct += classify_prompt(text, probability_of)[1] == sport
+
+    print(f"Acuratetea modelului este de {correct / count * 100:.2f}%.")
+
+calculate_accuracy()
