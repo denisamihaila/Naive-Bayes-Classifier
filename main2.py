6b24c6c528d7dadea1e0ad61402c9cae67f83ba7
bgd <trifan.bogdan09@yahoo.com>
2024-11-10 16:21:13 +0200

    aproximativ Bayes

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 import json
+from collections import Counter
 
 # functia read_data() citeste articolele din json-ul articole.json
 # si face un dictionar de forma sport_dict[nume_sport] = [ cuvinte ]
@@ -9,6 +10,8 @@ import json
 def read_data():
     fin = open('articole.json', 'r', encoding='utf-8')
     data = json.load(fin)
+    fin.close()
+
     sport_dict = dict()
     delimiters = [",", ";", " ", "'", "’", "-", "."]
     for sport in data:
@@ -24,4 +27,56 @@ def read_data():
 
     return sport_dict
 
-read_data()
+# functia primeste dictionarul cu cuvinte
+# si calculeaza probabilitatea fiecare cuvant
+# sa apara intr-o categorie de sport
+def calculate_probabilities_word(sport_dict):
+    word_prob = dict()
+    for sport in sport_dict: # football, handball, tennis, basketball
+        word_prob[sport] = dict()
+        total_words = len(sport_dict[sport])
+        used_words = set()
+
+        for word in sport_dict[sport]:
+            if word not in used_words:
+                appearances = sport_dict[sport].count(word)
+                word_prob[sport][word] = appearances / total_words
+                used_words.add(word)
+    return word_prob
+
+def classify_text(text, probs):
+    delimiters = [",", ";", " ", "'", "’", "-", "."]
+    for delimiter in delimiters:
+        text = text.replace(delimiter, " ")
+    words = [word.lower() for word in text.split() if len(word) > 2]
+
+    # P(category)
+    p_category = 1 / len(probs)
+
+    result = (0, "")
+
+    for category in probs:
+
+        # Compute P(word)
+        P_word = 0.0001
+        for word in words:
+            P_word += words.count(word) / len(words)
+
+        # Compute P(word | category)
+        P_word_given_category = 0.0001
+        for word in words:
+            if word in probs[category]:
+                P_word_given_category += probs[category][word]
+
+        # Compute P(category | word)
+        p_category_given_word = P_word_given_category * p_category / P_word
+
+        if p_category_given_word >= result[0]:
+            result = (p_category_given_word, category)
+
+    return result
+
+data = read_data()
+probs = calculate_probabilities_word(data)
+res = classify_text("I've won this tennis match using my FIFA Manchester City team", probs)
+print(f"Textul apartine categoriei {res[1]} avand probabilitatea de {res[0] * 100:.2f}%.")
